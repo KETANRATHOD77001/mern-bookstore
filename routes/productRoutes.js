67a5f2849053c4dad0cdf1cb8028adf2680f1e49
KETANRATHOD77001 <ketanketan77001@gmail.com>
2023-04-25 00:09:58 +0530

    final code
    bookstore mern stack app

---
+++ routes/productRoutes.js
@@ -0,0 +1,69 @@
+import express from "express";
+import {
+  createProductController,
+  deleteProductController,
+  getProductController,
+  getSingleProductController,
+  productCountController,
+  productFiltersController,
+  productListController,
+  productPhotoController,
+  updateProductController,
+  PaymentController,
+  updateRating,
+  productRatingFiltersController,
+} from "../controllers/productController.js";
+import { isAdmin, requireSignIn } from "../middlewares/authMiddleware.js";
+import formidable from "express-formidable";
+
+const router = express.Router();
+
+//routes
+router.post(
+  "/create-product",
+  requireSignIn,
+  isAdmin,
+  formidable(),
+  createProductController
+);
+//routes
+router.put(
+  "/update-product/:pid",
+  requireSignIn,
+  isAdmin,
+  formidable(),
+  updateProductController
+);
+
+//get products
+router.get("/get-product", getProductController);
+
+//single product
+router.get("/get-product/:slug", getSingleProductController);
+
+//get photo
+router.get("/product-photo/:pid", productPhotoController);
+
+//delete rproduct
+router.delete("/delete-product/:pid", deleteProductController);
+
+//filter product
+router.post("/product-filters", productFiltersController);
+
+//filter product
+router.post("/product-filters-rating", productRatingFiltersController);
+
+//product count
+router.get("/product-count", productCountController);
+
+//product per page
+router.get("/product-list/:page", productListController);
+
+
+//payments
+router.post("/payment", requireSignIn, PaymentController);
+
+//rating
+router.get("/:id/rating/:rating/", updateRating);
+
+export default router;
